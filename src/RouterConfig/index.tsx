789e9262a022c726e0080163d27423b0ec606804
LeonAvancini <avancinileon@gmail.com>
2022-06-22 22:24:56 +0200

    creating basic project structure

---
+++ src/RouterConfig/index.tsx
@@ -0,0 +1,14 @@
+import Footer from "../layout/Fotter";
+import Header from "../layout/Header";
+
+const RouterConfig = () => {
+  return (
+    <>
+      <Header />
+      <main>content</main>
+      <Footer />
+    </>
+  );
+};
+
+export default RouterConfig;
