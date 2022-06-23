1696e519021852f65e2dd5cf2b1d360188fe1424
LeonAvancini <avancinileon@gmail.com>
2022-06-23 09:12:32 +0200

    added colors and styles on header

--- src/layout/Header/index.tsx
+++ src/layout/Header/index.tsx
@@ -1,5 +1,29 @@
+import styled from "styled-components";
+import { primary } from "../../utils/colors";
+
+const Container = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 4rem;
+  padding: 0.5rem;
+  background-color: ${primary};
+`;
+
+const LogoImage = styled.img`
+  width: 50%;
+  height: auto;
+`;
+
 const Header = () => {
-  return <div>header</div>;
+  return (
+    <Container>
+      <LogoImage
+        src="http://piedemonteclub.com.ar/wp-content/uploads/2021/09/logo_pdm-min.png"
+        alt="logo"
+      />
+    </Container>
+  );
 };
 
 export default Header;
---
+++ src/utils/colors.ts
@@ -0,0 +1,8 @@
+export const primary = "#D68A3E";
+export const secondary = "#518338";
+export const black = "#000000";
+export const white = "#FFFFFF";
+export const gray = "#F8F8F8";
+export const darkGray = "#767676";
+export const lightGray = "#DBDBDB";
+export const red = "#E67675";
